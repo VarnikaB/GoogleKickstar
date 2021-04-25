6cf209055b6df92c3d4bf544170176ef3d34044a
VarnikaB <55554547+VarnikaB@users.noreply.github.com>
2021-04-25 21:46:49 +0530

    Small python snippets

---
+++ Pythoncode.py
@@ -0,0 +1,59 @@
+#Python codes
+
+#Prime number-----
+flag = False
+
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
+#-------------------
+#factorial----------
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while(n > 1):
+            fact *= n
+            n -= 1
+        return fact
+#--------------------
+#Sieve method--------
+def SieveOfEratosthenes(n):
+      
+    # Create a boolean array "prime[0..n]" and initialize
+    # all entries it as true. A value in prime[i] will
+    # finally be false if i is Not a prime, else true.
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+          
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True):
+              
+            # Update all multiples of p
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime[0]= False
+    prime[1]= False
+#----------------------
+#Anagram---------------
+from collections import Counter
+
+def anagram(first, second):
+  return Counter(first) == Counter(second)
+#----------------------
